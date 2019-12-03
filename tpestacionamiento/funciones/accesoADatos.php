6473c5bcd3ff0dfb79bc3a2b5e562c710f09705d
mauricioganu <54649732+mauricioganu@users.noreply.github.com>
2019-12-02 21:29:23 -0300

    Add files via upload

--- tpestacionamiento/funciones/accesoADatos.php
+++ tpestacionamiento/funciones/accesoADatos.php
@@ -7,7 +7,7 @@ class AccesoDatos
     private function __construct()
     {
         try { 
-            $this->objetoPDO = new PDO('mysql:host=remotemyswl.com;dbname=cgKTsxigD9;charset=utf8', 'cgKTsxigD9', 'LRSTvkUm7', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $this->objetoPDO = new PDO('mysql:host=remotemysql.com;dbname=cgKTsxigD9;charset=utf8', 'cgKTsxigD9', 'LRSTvkUm7', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             $this->objetoPDO->exec("SET CHARACTER SET utf8");
             } 
         catch (PDOException $e) { 
